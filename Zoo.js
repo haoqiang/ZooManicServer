4d2149cb4535c01b8b9e430ad78561f6f258a88c
Ubuntu <ubuntu@ip-10-238-135-245.ec2.internal>
2013-10-22 08:19:06 +0000

    change zoomap

--- Zoo.js
+++ Zoo.js
@@ -1,10 +1,10 @@
 var Zoo = {
 	TREE_PERCENTAGE : 50,	// percentage of trees in the map
 	ITEM_PERCENTAGE : 50,	// percentage of items over the tree
-	ZOO_WIDTH : 28,			// number of horizontal cells
+	ZOO_WIDTH : 38,			// number of horizontal cells
 	ZOO_HEIGHT : 28,		// number of vertical cells
-	CELL_WIDTH : 30,		// width of a cell in px
-	CELL_HEIGHT : 30,		// height of a cell in px
+	CELL_WIDTH : 3.4,		// width of a cell in px
+	CELL_HEIGHT : 3,		// height of a cell in px
 	FRAME_RATE : 25,		// frame rate of the game
 	PORT : 5000,
 	SERVER_IP: '54.225.24.113',
