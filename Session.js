0e61bbfda5c80c935751728bd77f63344d204d38
Ubuntu <ubuntu@ip-10-238-135-245.ec2.internal>
2013-10-26 04:39:38 +0000

    Please enter the commit message for your changes. Lines starting

--- Session.js
+++ Session.js
@@ -88,7 +88,7 @@ function Session(sid) {
             }
 
             // put players position inside the message
-            states.players = {};
+            // states.players = {};
             // for (var i = 0; i < players.length; i++) {
             //     states.players[players[i].id] = {x: players[i].x, y: players[i].y, bombLeft: players[i].bombLeft};
             // }
