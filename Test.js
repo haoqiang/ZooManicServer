67d84b6918993bf9501474b75f2615fb353fea81
Haoqiang <mail.haoqiang@gmail.com>
2013-11-02 15:51:58 +0800

    Revert test.js

--- Test.js
+++ Test.js
@@ -18,7 +18,9 @@ function TestClient() {
 		if(playerId!==undefined){
 			msg.playerId = playerId;
 		}
-		document.getElementById("output").innerHTML += "<hr>outgoing:<br><pre>"+JSON.stringify(msg, null, 4)+"</pre>";
+		if(msg.type!=="ping"){
+			$("#output").prepend("<hr>outgoing:<br><pre>"+JSON.stringify(msg, null, 4)+"</pre>");
+		}
 		socket.send(JSON.stringify(msg));
 	}
 
@@ -31,25 +33,24 @@ function TestClient() {
 	this.initNetwork = function () {
 		// Attempts to connect to game server
 		try {
-			//socket = new SockJS("http://" + Zoo.SERVER_NAME + ":" + Zoo.PORT + "");
-			if(location.host!==""){
+			if(location.host !== ""){
 				socket = new SockJS("http://"+location.host+":" + Zoo.PORT + "");
+				//socket = new SockJS("http://" + Zoo.SERVER_NAME + ":" + Zoo.PORT + "");
 			}else{
 				socket = new SockJS("http://localhost:" + Zoo.PORT + "");
 			}
 			socket.onmessage = function (e) {
 				var message = JSON.parse(e.data);
 
-				if(message.type!=="update"){
-					document.getElementById("output").innerHTML += "<hr>incoming:<br><pre>"+JSON.stringify(message, null, 4)+"</pre>";
-				}
 				if(message.type==="newPlayerReply"){
 					playerId = message.playerId;
+					$("#newPlayer").html("Success: new player created with id: " + playerId);
+				}else if(message.type!=="update" && message.type!=="ping"){
+					$("#output").prepend("<hr>incoming:<br><pre>"+JSON.stringify(message, null, 4)+"</pre>");
 				}
 				if(message.type==="ping"){
 					that.sendToServer(message);
 				}
-
 			};
 		} catch (e) {
 			console.log("Failed to connect");
@@ -76,67 +77,41 @@ function TestClient() {
 	}
 }
 
+var delay = 3000;
+var interval = 500;
+var test = new TestClient();
+test.start();
+setTimeout(function(){
+	test.sendToServer({type:"newPlayer", playerName: "TestPlayer-"+Math.floor((Math.random()*100)+1) });
+}, delay);
+
+
 $(document).ready(function(){
-	var test = new TestClient();
-	test.start();
 
-
-	var delay = 1000;
-	var interval = 500;
-	setTimeout(function(){
-		document.getElementById("output").innerHTML += "<p><b>Step 1: Request server to create new player</b></p>";
-		test.sendToServer({type:"newPlayer", playerName: "test player"});
-	}, delay);
-	// delay += interval;
-	// setTimeout(function(){
-	// 	document.getElementById("output").innerHTML += "<p><b>Step 2: Get all available game rooms</b></p>";
-	// 	test.sendToServer({type:"getAllSession"});
-	// }, delay);
-	// delay += interval;
-	// setTimeout(function(){
-	// 	document.getElementById("output").innerHTML += "<p><b>Step 3: Join a game rooms</b></p>";
-	// 	test.sendToServer({type:"setSession", sessionId:"100000"});
-	// }, delay);
-	// delay += interval;
-	// setTimeout(function(){
-	// 	document.getElementById("output").innerHTML += "<p><b>Step 4: Start the game</b></p>";
-	// 	test.sendToServer({type:"start"});
-	// }, delay);
-	// delay += interval;
-	// setTimeout(function(){
-	// 	document.getElementById("output").innerHTML += "<p><b>Step 5: Make some move</b></p>";
-	// 	test.sendToServer({type:"move", x: 5, y: 5});
-	// }, delay);
-	// delay += interval;
-	// setTimeout(function(){
-	// 	document.getElementById("output").innerHTML += "<p><b>Step 6: Try disconnect</b></p><hr>";
-	// 	test.disconnectNetwork();
-	// }, delay);
-	// delay += interval;
-	// setTimeout(function(){
-	// 	document.getElementById("output").innerHTML += "<p><b>Step 8: Try reconnect</b></p><hr>";
-	// 	test.initNetwork();
-	// }, delay);
-	// delay += interval;
-	// setTimeout(function(){
-	// 	document.getElementById("output").innerHTML += "<p><b>Step 9: Try make some move</b></p>";
-	// 	test.sendToServer({type:"move", x: 51, y: 51});
-	// }, delay);
-	// delay += interval;
-	// setTimeout(function(){
-	// 	document.getElementById("output").innerHTML += "<p><b>Step 9: Try make some move again</b></p>";
-	// 	test.sendToServer({type:"move", x: 51, y: 51});
-	// }, delay);
-	// delay += interval;
-
-
-
-
-	$("#getAllSession").on("click", function(){
+	$("#getAllPlayer").on("click", function(){
 		test.sendToServer({type:"getAllPlayerStats"});
 	});
 
+	$("#getAllSession").on("click", function(){
+		test.sendToServer({type:"getAllSession"});
+	});
 
+	$("#setSession").on("click", function(){
+		var sid = $("#sessionId").val();
+		if($.isNumeric(sid)){
+			test.sendToServer({type:"setSession", sessionId: sid});
+		}
+	});
+
+	$("#ready").on("click", function(){
+		var avatarId = $("#avatarId").val();
+		avatarId = $.isNumeric(avatarId)?avatarId:0;
+		test.sendToServer({type:"playerReady", avatarId: avatarId});
+	});
+
+	$("#start").on("click", function(){
+		test.sendToServer({type:"start"});
+	});
 
 	$("#move").on("click", function(){
 		test.sendToServer({type:"move", cellX: 5, cellY: 5, direction: "UP"});
@@ -144,5 +119,49 @@ $(document).ready(function(){
 
 
 
-
 });
+
+function automatedTest(){
+
+	delay += interval;
+	setTimeout(function(){
+		document.getElementById("output").innerHTML += "<p><b>Step 2: Get all available game rooms</b></p>";
+		test.sendToServer({type:"getAllSession"});
+	}, delay);
+	delay += interval;
+	setTimeout(function(){
+		document.getElementById("output").innerHTML += "<p><b>Step 3: Join a game rooms</b></p>";
+		test.sendToServer({type:"setSession", sessionId:"100000"});
+	}, delay);
+	delay += interval;
+	setTimeout(function(){
+		document.getElementById("output").innerHTML += "<p><b>Step 4: Start the game</b></p>";
+		test.sendToServer({type:"start"});
+	}, delay);
+	delay += interval;
+	setTimeout(function(){
+		document.getElementById("output").innerHTML += "<p><b>Step 5: Make some move</b></p>";
+		test.sendToServer({type:"move", x: 5, y: 5});
+	}, delay);
+	delay += interval;
+	setTimeout(function(){
+		document.getElementById("output").innerHTML += "<p><b>Step 6: Try disconnect</b></p><hr>";
+		test.disconnectNetwork();
+	}, delay);
+	delay += interval;
+	setTimeout(function(){
+		document.getElementById("output").innerHTML += "<p><b>Step 8: Try reconnect</b></p><hr>";
+		test.initNetwork();
+	}, delay);
+	delay += interval;
+	setTimeout(function(){
+		document.getElementById("output").innerHTML += "<p><b>Step 9: Try make some move</b></p>";
+		test.sendToServer({type:"move", x: 51, y: 51});
+	}, delay);
+	delay += interval;
+	setTimeout(function(){
+		document.getElementById("output").innerHTML += "<p><b>Step 9: Try make some move again</b></p>";
+		test.sendToServer({type:"move", x: 51, y: 51});
+	}, delay);
+	delay += interval;
+}
